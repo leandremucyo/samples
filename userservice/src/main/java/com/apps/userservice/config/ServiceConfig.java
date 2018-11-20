a746b2afc43c0ed0095f7ab8093495ef3cf3c41b
Leandre M <leandremucho@gmail.com>
2018-11-19 19:26:04 -0500

    Upgrading to Spring 2.0.0.RELEASE & Adding auctuator refresh endpoint since it is not on by default

--- userservice/src/main/java/com/apps/userservice/config/ServiceConfig.java
+++ userservice/src/main/java/com/apps/userservice/config/ServiceConfig.java
@@ -2,10 +2,10 @@ package com.apps.userservice.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
 @RefreshScope
+@Service
 public class ServiceConfig {
 
 	@Value("${message}")
